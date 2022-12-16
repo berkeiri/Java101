3d9a65a185b5c856723a45aa68e28c09e54255f8
berkeiri <bquass07@gmail.com>
2022-12-16 19:40:38 +0300

    first commit

---
+++ src/AsalSayi.java
@@ -0,0 +1,16 @@
+public class AsalSayi {
+    public static void main(String[] args) {
+        int i;
+        for (i=1;i<=100;i++){
+            if (i%2==0){
+                System.out.println(",");
+            } else if (i%3==0) {
+                System.out.println(",");
+            }else {
+                System.out.println(i+"asal");
+            }
+
+        }
+
+    }
+}
