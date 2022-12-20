cfa590e12959859ff3649a2362a7d564d5cd4d96
berkeiri <bquass07@gmail.com>
2022-12-20 19:52:45 +0300

    first commit

---
+++ src/OgrenciBilgi/Student.java
@@ -0,0 +1,92 @@
+package OgrenciBilgi;
+
+public class Student {
+    String name,stuNo;
+    int classes;
+    Course mat;
+    Course fizik;
+    Course kimya;
+    double avarage;
+    boolean isPass;
+    double ortalama;
+
+
+
+    Student(String name, int classes, String stuNo, Course mat,Course fizik,Course kimya) {
+        this.name = name;
+        this.classes = classes;
+        this.stuNo = stuNo;
+        this.mat = mat;
+        this.fizik = fizik;
+        this.kimya = kimya;
+        calcAvarage();
+        this.isPass = false;
+    }
+
+    public void addSozluExamNote(int mat, int fizik, int kimya){
+        this.mat.sozlu=mat;
+        this.fizik.sozlu=fizik;
+        this.kimya.sozlu=kimya;
+    }
+    public void gectiMi(){
+
+    }
+    public void ortalama(){
+
+    }
+
+
+    public void addBulkExamNote(int mat, int fizik, int kimya) {
+
+        if (mat >= 0 && mat <= 100) {
+            this.mat.note = mat;
+        }
+
+        if (fizik >= 0 && fizik <= 100) {
+            this.fizik.note = fizik;
+        }
+
+        if (kimya >= 0 && kimya <= 100) {
+            this.kimya.note = kimya;
+        }
+
+    }
+    public void yazdir(){
+        System.out.println(this.mat.sozlu);
+        System.out.println(this.fizik.sozlu);
+        System.out.println(this.kimya.sozlu);
+    }
+
+    public void isPass() {
+        if (this.mat.note == 0 || this.fizik.note == 0 || this.kimya.note == 0) {
+            System.out.println("Notlar tam olarak girilmemiş");
+        } else {
+            this.isPass = isCheckPass();
+            printNote();
+            System.out.println("Ortalama : " + this.avarage);
+            if (this.isPass) {
+                System.out.println("Sınıfı Geçti. ");
+            } else {
+                System.out.println("Sınıfta Kaldı.");
+            }
+        }
+    }
+
+    public void calcAvarage() {
+        this.avarage = ((this.fizik.note*0.80)+(this.fizik.sozlu*0.20)+(this.mat.note*0.80)+(this.mat.sozlu*0.20)+(this.fizik.note*0.80)+(this.fizik.sozlu*0.20))/3;
+    }
+
+    public boolean isCheckPass() {
+        calcAvarage();
+        return this.avarage > 55;
+    }
+
+    public void printNote(){
+        System.out.println("=========================");
+        System.out.println("Öğrenci : " + this.name);
+        System.out.println("Matematik Notu : " + this.mat.note);
+        System.out.println("Fizik Notu : " + this.fizik.note);
+        System.out.println("Kimya Notu : " + this.kimya.note);
+    }
+
+}
